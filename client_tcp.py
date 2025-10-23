73357391827627a2522ef829638df583ad194d6c
yasss14 <yahyaouiyassine05@gmail.com>
2025-10-23 12:14:05 -0400

    final

--- client_tcp.py
+++ client_tcp.py
@@ -1,5 +1,4 @@
-# Source : https://pymotw.com/2/socket/tcp.html
-
+# ...existing code...
 import socket
 import sys
 
@@ -7,26 +6,23 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = ('10.20.61.12', 10000)  # remplacer par l'IP de l'ESP si différente
 print('Connexion TCP a %s sur le port %s' % server_address)
 sock.connect(server_address)
 
 try:
     
-    # Send data
-    message = 'Ceci est un messsage. Il va etre repete par le serveur.'
+    # Send command to toggle LED
+    message = 'LED ON'  # ou 'LED OFF'
     print('Envoi de : "%s"' % message)
-    sock.sendall(message.encode())
+    sock.sendall((message + '\n').encode())
     
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(255)
-        amount_received += len(data)
-        print('Recu :  "%s"' % data.decode())
+    # Wait for response (single line)
+    data = sock.recv(255)
+    if data:
+        print('Recu :  "%s"' % data.decode().strip())
 
 finally:
     print('Fermeture du socket TCP')
     sock.close()
+# ...existing code...
